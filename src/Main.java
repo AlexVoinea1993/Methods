7dca2e05be3e14b2113bcc34060919d70a4d0098
alexv <alexvoinea1@gmail.com>
2023-10-09 22:18:04 +0300

    Added first method

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Mathematics.addTwoNumbers(12, 14);
+        Mathematics.addTwoNumbers(22, 31);
+    }
+}
---
+++ src/Mathematics.java
@@ -0,0 +1,6 @@
+public class Mathematics {
+
+    public static void addTwoNumbers(int x, int y){
+        System.out.println(x + y);
+    }
+}
