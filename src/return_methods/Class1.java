6292f81c151009913867dd6d00cd0f6a7184628b
alexv <alexvoinea1@gmail.com>
2023-10-16 22:30:29 +0300

    Added seventh method

--- src/return_methods/Class1.java
+++ src/return_methods/Class1.java
@@ -2,8 +2,6 @@ package return_methods;
 
 public class Class1 {
 
-//        Scrie o metoda care primeste 2 numere si il returneaza pe cel mai mare
-
     public static int findMin(int number1, int number2) {
         if (number1 > number2) {
             return number1;
@@ -11,8 +9,6 @@ public class Class1 {
             return number2;
         }
     }
-
-//    Scrie 1 metoda care primeste 1 numar, si returneaza  pozitiv sau negativ
 
     public static String findPositiveNumber(int number) {
         if (number > 0) {
@@ -22,15 +18,11 @@ public class Class1 {
         }
     }
 
-//    Scrie 1 metoda care primeste 5 numbere double si returneaza media lor
-
     public static double averageOfAllNumbers(double number1, double number2, double number3, double number4,
                                              double number5) {
         double sum = number1 + number2 + number3 + number4 + number5;
         return sum / 5;
     }
-
-//    Scrie 1 metoda playFizzBuzz care primeste 1 numar si returneaza Fizz, Buzz, sau FizzBuzz
 
     public static String fizzBuzzGame(int number) {
         if (number % 5 == 0 && number % 3 == 0) {
