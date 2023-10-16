5fe772409187cc1658eec039f33f5ba2abc0711c
alexv <alexvoinea1@gmail.com>
2023-10-16 21:41:31 +0300

    Added sixth method

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,25 @@
 public class Main {
     public static void main(String[] args) {
 
-        Mathematics.addTwoNumbers(12, 14);
-        Mathematics.addTwoNumbers(22, 31);
-        Mathematics.multiplyTwoNumbers(12, 22);
-        Mathematics.averageOfThreeNumbers(2, 3, 5);
-        Mathematics.squareNumber(22);
-        Mathematics.findBiggestNumber(22, 41);
+        Mathematics_Void.addTwoNumbers(12, 14);
+        Mathematics_Void.addTwoNumbers(22, 31);
+        Mathematics_Void.multiplyTwoNumbers(12, 22);
+        Mathematics_Void.averageOfThreeNumbers(2, 3, 5);
+        Mathematics_Void.squareNumber(22);
+        Mathematics_Void.findBiggestNumber(22, 41);
+        Mathematics_Void.playFizzBuzz(30);
+        Mathematics_Void.positiveAndNegativeNumbers(1);
+        Mathematics_Void.multiplicationTable(5);
+        Mathematics_Void.positiveAndNegativeNumbers(-4);
+        Text_Void.monthsAndSeasonsIF("Octombrie");
+        Text_Void.monthsAndSeasonSwitch("March");
+        Homework.theBiggerNumber(10, 6, 5);
+        Homework.theBiggerNumber(5, 6, 10);
+        Homework.theBiggerNumber(6, 10, 5);
+        Homework.waterState(101);
+        Homework.theSmallestNumber(2, 10, 30, 1);
+        Homework.theSmallestNumber(1, 30, 10, 2);
+        Homework.theSmallestNumber(30, 10, 2, 1);
+        Homework.theSmallestNumber(10, 2, 1, 30);
     }
 }
---
+++ src/Text_Void.java
@@ -0,0 +1,44 @@
+public class Text_Void {
+
+//    //    Scrie o metoda care primeste 1 parametru String luna si afiseaza anotimpul corespunzator,
+//          lunii respective
+
+
+    public static void monthsAndSeasonsIF(String month) {
+        if (month.equalsIgnoreCase("Ianuarie") || month.equalsIgnoreCase("Februarie")
+                || month.equalsIgnoreCase("Decembrie")) {
+            System.out.println("Anotimpul corespunzator lunii respective este iarna");
+        } else if (month.equalsIgnoreCase("Martie") || month.equalsIgnoreCase("Aprilie")
+                || month.equalsIgnoreCase("Mai")) {
+            System.out.println("Anotimpul corespunzator lunii respective este primavara");
+        } else if (month.equalsIgnoreCase("Iunie") || month.equalsIgnoreCase("Iulie")
+                || month.equalsIgnoreCase("August")) {
+            System.out.println("Anotimpul corespunzator lunii respective este, vara");
+        } else if (month.equalsIgnoreCase("Septembrie") || month.equalsIgnoreCase("Octombrie")
+                || month.equalsIgnoreCase("Noiembrie")) {
+            System.out.println("Anotimpul corespunzator lunii respective este, toamna");
+        } else {
+            System.out.println("Anotimpul nu exista");
+        }
+    }
+
+    public static void monthsAndSeasonSwitch(String months) {
+        switch (months) {
+            case "December", "January", "February":
+                System.out.println("The following months are part of the winter season");
+                break;
+            case "March", "April", "Mai":
+                System.out.println("The following months are part of the spring season");
+                break;
+            case "June", "July", "August":
+                System.out.println("The following months are part of the summer season");
+                break;
+            case "September", "October", "November":
+                System.out.println("The following months are part of the autumn season");
+                break;
+            default:
+                System.out.println("Invalid season");
+                break;
+        }
+    }
+}
